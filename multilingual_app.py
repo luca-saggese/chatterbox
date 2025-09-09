7a16b1c5df80a8d4cb5236e5933b3f78b7374bd0
Luca Saggese <luca.saggese@gmail.com>
2025-09-09 11:50:45 +0200

    fix

--- multilingual_app.py
+++ multilingual_app.py
@@ -314,4 +314,4 @@ with gr.Blocks() as demo:
         outputs=[audio_output],
     )
 
-demo.launch(mcp_server=True, host="0.0.0.0", port=8080, share=True)
+demo.launch(mcp_server=True, server_name="0.0.0.0", port=8080, share=True)
